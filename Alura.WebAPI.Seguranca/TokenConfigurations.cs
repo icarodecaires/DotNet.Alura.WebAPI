45c83514a4b6dfcf5fc3f864b66f48aaa770be38
Icaro Caires <icaro.caires@interplayers.com.br>
2021-09-02 17:55:07 -0300

    testee

--- Alura.WebAPI.Seguranca/TokenConfigurations.cs
+++ Alura.WebAPI.Seguranca/TokenConfigurations.cs
@@ -5,6 +5,8 @@
         public string Audience { get; set; }
         public string Issuer { get; set; }
         public int Seconds { get; set; }
+
+        //testando
     }
      
      //fazendo algo
